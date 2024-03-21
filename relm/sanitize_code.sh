34d8fe9394005687f0f2c0c719f6815de600914c
eylkc <164383026+eylkc@users.noreply.github.com>
2024-03-21 18:09:05 -0500

    Delete relm/sanitize_code.sh

--- relm/sanitize_code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-autopep8 --in-place --aggressive --aggressive --aggressive python/relm/**/*.py
-autopep8 --in-place --aggressive --aggressive --aggressive tests/**/*.py
-isort python/relm/.
-isort tests/.
